60b24ab7fca7cc4733336c9a0336e607df6ba666
Elsa Dwi Agresty <112572323+Elsadwiagresty07@users.noreply.github.com>
2022-09-22 01:00:47 +0700

    Add files via upload

---
+++ ElsaDwiAgresty_posttest1_C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int MyNumb = 7;
+	cout << MyNumb<<endl;
+	
+	float MyFloatNumb = 13.06;
+	cout << MyFloatNumb<<endl;
+	
+	string MyMotivate = "Love Yourself";
+	cout << MyMotivate<<endl;
+	
+	char MyFavorite = 'j';
+	cout << MyFavorite<<endl;
+	
+	return 0;
+}
